8de83380533ae3c6ee6eb4d5892ed674db46be65
djallel red <djallel@kali.djallel>
2026-06-01 12:46:22 -1000

    fix port

--- app.py
+++ app.py
@@ -69,4 +69,4 @@ def generate():
     return jsonify({ "result": result })
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
